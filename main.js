2ffcbd356fd7d71e2389f161a212a97f8e1aac83
Krob <koen.roekens@gmail.com>
2025-07-02 09:42:10 +0000

    changes in main.js, styling pmtiles protocol, ..

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 import * as maplibregl from "https://esm.sh/maplibre-gl";
+import { Protocol } from "https://esm.sh/pmtiles";
+
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    //style: 'https://demotiles.maplibre.org/style.json', // style URL
+    style: './assets/style.json',
+    center: [51.96857, 5.66509],
+    zoom: 13 // starting zoom
 });
+
+
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
