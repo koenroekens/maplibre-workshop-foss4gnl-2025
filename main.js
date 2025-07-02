fe1968adbb04a7c21fa916a467ca1b3d9bdcd14a
Krob <koen.roekens@gmail.com>
2025-07-02 09:08:01 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import * as maplibregl from "https://esm.sh/maplibre-gl";
+
+const map = new maplibregl.Map({
+    container: 'mijnkaart', // container id
+    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
